f3eb23ae11f6c87b7af03d036bb0e6f000694aa0
CodingwithJess <thejessiebess@gmail.com>
2020-08-17 19:47:46 -0600

    styling update

--- public/index.js
+++ public/index.js
@@ -71,7 +71,7 @@ function populateChart() {
         datasets: [{
             label: "Total Over Time",
             fill: true,
-            backgroundColor: "#6666ff",
+            backgroundColor: "#f39603",
             data
         }]
     }
